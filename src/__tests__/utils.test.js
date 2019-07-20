53828cbbf8b06e19850e14b4e1c5e556a309e04c
Duncan Bay <duncan.bay@gmail.com>
2019-07-20 17:56:03 +1000

    fix: handle older kindle paperwhite 2013 format

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,4 @@
-const {
-  single,
-  multiple,
-  withDuplicates,
-  withBookmarks,
-} = require('./fixtures');
+const { single, multiple, withDuplicates, withBookmarks, alternateFormat } = require('./fixtures');
 
 const {
   contractSpaces,
@@ -17,10 +12,11 @@ const {
   prependEllipsis,
   prependZero,
   reorderNames,
-  safeMatch,
+  match,
   smartQuotes,
   stripBOM,
   swapJoin,
+  to24Hr,
   transformQuotes,
 } = require('../utils');
 
@@ -36,23 +32,35 @@ describe('monthNameToNum()', () => {
   });
   it('works as expected', () => {
     const expected = [
-      'January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December',
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
     ];
     const actual = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     expect(expected.map(monthNameToNum)).toEqual(expect.arrayContaining(actual));
   });
 });
 
-describe('safeMatch()', () => {
+describe('match()', () => {
   it('has a sane default', () => {
-    expect(safeMatch()).toEqual([]);
+    expect(match()).toEqual([]);
   });
   it('returns matches', () => {
-    expect(safeMatch('anapple', /(an)(apple)/)).toEqual(expect.arrayContaining(['anapple', 'an', 'apple']));
+    expect(match('anapple', /(an)(apple)/)).toEqual(
+      expect.arrayContaining(['anapple', 'an', 'apple'])
+    );
   });
   it('returns empty array instead of null if no match', () => {
-    expect(safeMatch('anapple', /oranges/)).toEqual([]);
+    expect(match('anapple', /oranges/)).toEqual([]);
   });
 });
 
@@ -130,8 +138,9 @@ describe('parseTitle()', () => {
     expect(parseTitle()).toBe('');
   });
   it('works as expected', () => {
-    expect(parseTitle('Acceptance: A Novel (The Southern Reach Trilogy) (Jeff VanderMeer)'))
-      .toBe('Acceptance: A Novel (The Southern Reach Trilogy)');
+    expect(parseTitle('Acceptance: A Novel (The Southern Reach Trilogy) (Jeff VanderMeer)')).toBe(
+      'Acceptance: A Novel (The Southern Reach Trilogy)'
+    );
   });
 });
 
@@ -140,10 +149,12 @@ describe('parseAuthors()', () => {
     expect(parseAuthors()).toEqual([]);
   });
   it('works as expected', () => {
-    expect(parseAuthors('Acceptance: A Novel (The Southern Reach Trilogy) (Jeff VanderMeer)'))
-      .toEqual(expect.arrayContaining(['Jeff VanderMeer']));
-    expect(parseAuthors('Songs of the Dying Earth (Dozois, Gardner;Martin, George R.R.)'))
-      .toEqual(expect.arrayContaining(['Gardner Dozois', 'George R.R. Martin']));
+    expect(
+      parseAuthors('Acceptance: A Novel (The Southern Reach Trilogy) (Jeff VanderMeer)')
+    ).toEqual(expect.arrayContaining(['Jeff VanderMeer']));
+    expect(parseAuthors('Songs of the Dying Earth (Dozois, Gardner;Martin, George R.R.)')).toEqual(
+      expect.arrayContaining(['Gardner Dozois', 'George R.R. Martin'])
+    );
   });
 });
 
@@ -152,8 +163,11 @@ describe('parseDate()', () => {
     expect(parseDate()).toBe('');
   });
   it('works as expected', () => {
-    expect(parseDate('- Your Highlight at location 11849-11850 | Added on Thursday, 30 April 2015 12:58:20'))
-      .toEqual('2015-04-30T12:58:20.000Z');
+    expect(
+      parseDate(
+        '- Your Highlight at location 11849-11850 | Added on Thursday, 30 April 2015 12:58:20'
+      )
+    ).toEqual('2015-04-30T12:58:20.000Z');
   });
 });
 
@@ -162,8 +176,25 @@ describe('parseLoc()', () => {
     expect(parseLoc()).toBe('');
   });
   it('works as expected', () => {
-    expect(parseLoc('- Your Highlight at location 11849-11850 | Added on Thursday, 30 April 2015 20:58:20'))
-      .toBe('11849-11850');
+    expect(
+      parseLoc(
+        '- Your Highlight at location 11849-11850 | Added on Thursday, 30 April 2015 20:58:20'
+      )
+    ).toBe('11849-11850');
+  });
+});
+
+describe('to24Hr()', () => {
+  it('sane default', () => {
+    expect(to24Hr()).toBe('');
+  });
+  it('works as expected', () => {
+    expect(to24Hr('1:10:10 PM')).toBe('13:10:10');
+    expect(to24Hr('11:10:10 PM')).toBe('23:10:10');
+    expect(to24Hr('1:10:10 AM')).toBe('1:10:10');
+    expect(to24Hr('11:10:10 AM')).toBe('11:10:10');
+    expect(to24Hr('12:10:10 AM')).toBe('12:10:10');
+    expect(to24Hr('12:10:10 PM')).toBe('24:10:10');
   });
 });
 
@@ -172,11 +203,21 @@ describe('parseContent()', () => {
     expect(parseContent()).toBe('');
   });
   it('works as expected', () => {
-    expect(parseContent('Blake says, "Where?" He\'s a man who measures words as if he had only a few given to him by Fate; too generous a syllable from his lips, and he might fall over dead.'))
-      .toBe('Blake says, “Where?” He’s a man who measures words as if he had only a few given to him by Fate; too generous a syllable from his lips, and he might fall over dead.');
+    expect(
+      parseContent(
+        'Blake says, "Where?" He\'s a man who measures words as if he had only a few given to him by Fate; too generous a syllable from his lips, and he might fall over dead.'
+      )
+    ).toBe(
+      'Blake says, “Where?” He’s a man who measures words as if he had only a few given to him by Fate; too generous a syllable from his lips, and he might fall over dead.'
+    );
 
-    expect(parseContent('and eyeing the wizard speculatively across the room. A glance was enough to tell Molloqos that she was a woman of the evening, though in her case evening was edging on toward night.'))
-      .toBe('…and eyeing the wizard speculatively across the room. A glance was enough to tell Molloqos that she was a woman of the evening, though in her case evening was edging on toward night.');
+    expect(
+      parseContent(
+        'and eyeing the wizard speculatively across the room. A glance was enough to tell Molloqos that she was a woman of the evening, though in her case evening was edging on toward night.'
+      )
+    ).toBe(
+      '…and eyeing the wizard speculatively across the room. A glance was enough to tell Molloqos that she was a woman of the evening, though in her case evening was edging on toward night.'
+    );
   });
 });
 
@@ -195,5 +236,8 @@ describe('transformQuotes()', () => {
   });
   it('ignores bookmarks', () => {
     expect(transformQuotes(withBookmarks)).toMatchSnapshot();
+  });
+  it('handles alternate format', () => {
+    expect(transformQuotes(alternateFormat)).toMatchSnapshot();
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,12 @@
+const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
 const stripBOM = (str = '') => str.replace(/[\u200B-\u200D\uFEFF]/g, '');
 const swapJoin = (a = '', b = '') => `${b} ${a}`.trim();
+const first = (arr = []) => (Array.isArray(arr) ? arr[0] : null);
 const isEmpty = (x = '') => x == null || x.length === 0;
 const prependEllipsis = (str = '') => (/^[a-z]/.test(str) ? `…${str}` : str);
 const contractSpaces = (str = '') => str.replace(/\s{2,}/g, ' ');
-const safeMatch = (str = '', test = null) => str.match(test) || [];
-const reorderNames = (str = '') => /\w+, \w+/.test(str) ? swapJoin(...str.split(/,\s?/)) : str;
+const match = (str = '', test = null) => str.match(test) || [];
+const reorderNames = (str = '') => (/\w+, \w+/.test(str) ? swapJoin(...str.split(/,\s?/)) : str);
 const prependZero = (num = NaN) => {
   if (Number.isNaN(+num)) return '';
   return num < 10 ? `${0}${num}` : `${num}`;
@@ -27,45 +29,72 @@ function transformQuotes(str = '') {
 
       locs = locs.add(loc);
 
-      return list.concat({
+      const entry = {
         loc,
         title: parseTitle(header),
         authors: parseAuthors(header),
         date: parseDate(meta),
         content: parseContent(content),
-      });
+      };
+
+      return [...list, entry];
     }, []);
 }
 
 function parseTitle(str = '') {
-  return smartQuotes(safeMatch(str, /.+(?=\s\()/)[0]);
+  return compose(
+    smartQuotes,
+    first
+  )(match(str, /.+(?=\s\()/));
 }
 
 function parseAuthors(str = '') {
   // capture author names from string format: "Book title (maybe with parens) (AUTHOR NAMES)"
   const authorRE = /(?:\((?!.*\())(.+)(?:\))/;
-  const [, authors] = safeMatch(str, authorRE);
+  const [, authors] = match(str, authorRE);
 
   return authors ? authors.split(/;\s?/).map(reorderNames) : [];
 }
 
 function parseLoc(str = '') {
-  const [, loc] = safeMatch(str, /(?:location )(\d+-?\d+)/);
+  const [, loc] = match(str, /(?:location )(\d+-?\d+)/i);
   return loc || '';
 }
 
+function to24Hr(str) {
+  if (str == null) return '';
+  const time = str.split(/[:\s]/);
+  const isPM = time.pop() === 'PM';
+  return [+time.shift() + (isPM ? 12 : 0), ...time].join(':');
+}
+
 function parseDate(str = '') {
-  const [, dayNum, monthName, year, time] = safeMatch(
-    str,
-    /Added on \w+, (\d+) (\w+) (\d+) (.*)/,
-  );
+  let [, dayNum, monthName, year, time] = match(str, /Added on \w+, (\d+) (\w+) (\d+) (.*)/);
+  // alternate, older 2013 format
+  if (![dayNum, monthName, year, time].every(Boolean)) {
+    [, monthName, dayNum, year, time] = match(
+      str,
+      /Added on \w+, (\w+) (\d+), (\d{4}) (\d+:\d+:\d+ \w{2})/
+    );
+
+    time = to24Hr(time);
+  }
+
   const day = prependZero(dayNum);
-  const month = prependZero(monthNameToNum(monthName));
+  const month = compose(
+    prependZero,
+    monthNameToNum
+  )(monthName);
+
   return year ? `${year}-${month}-${day}T${time}.000Z` : '';
 }
 
 function parseContent(content = '') {
-  return smartQuotes(prependEllipsis(contractSpaces(content)));
+  return compose(
+    smartQuotes,
+    prependEllipsis,
+    contractSpaces
+  )(content);
 }
 
 function smartQuotes(str = '') {
@@ -82,13 +111,10 @@ function smartQuotes(str = '') {
       // backwards apostrophe
       .replace(
         /(\B|^)\u2018(?=([^\u2019]*\u2019\b)*([^\u2019\u2018]*\W[\u2019\u2018]\b|[^\u2019\u2018]*$))/gi,
-        '$1\u2019',
+        '$1\u2019'
       )
       // abbrev. years like '93
-      .replace(
-        /(\u2018)([0-9]{2}[^\u2019]*)(\u2018([^0-9]|$)|$|\u2019[a-z])/gi,
-        '\u2019$2$3',
-      )
+      .replace(/(\u2018)([0-9]{2}[^\u2019]*)(\u2018([^0-9]|$)|$|\u2019[a-z])/gi, '\u2019$2$3')
       .replace(/'/g, '\u2032')
   );
 }
@@ -123,9 +149,10 @@ module.exports = {
   prependEllipsis,
   prependZero,
   reorderNames,
-  safeMatch,
+  match,
   smartQuotes,
   stripBOM,
   swapJoin,
+  to24Hr,
   transformQuotes,
 };
